2718ed48174d5546271baf76841d79e684af4bbf
Vinodh1996 <vinodhs2233@gmail.com>
2016-10-06 11:42:42 +0530

    Add files via upload

---
+++ bean.java
@@ -0,0 +1,13 @@
+import java.awt.color;
+  private void jButton1MouseMoved(java.awt.event.MouseEvent){
+  jTextfield1.setBackground(color.red);
+  }
+  private void jButton2MouseClicked(java.awt.event.MouseEvent){
+  jTextfield4.setBackground(color.green);
+  }
+  private void jButton4ComponentResized(java.awt.event.ComponentEvent evt){
+  jTextfield8.setBackground(color.red);
+  }
+  private void jButton4ComponentResized(java.awt.event.ComponentEvent evt){
+  jTextfield8.setBackground(color.pink);
+  }
